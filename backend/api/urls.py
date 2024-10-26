5547a3e77d36ceac75385323e7e08cfba3cae91c
Артём Козлов <shtil14@mail.ru>
2024-10-26 16:00:58 +0300

    sort imports

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path, re_path
-from recipes.views import (CartViewSet, FavoriteViewSet, IngredientViewSet,
-                           RecipeViewSet, TagViewSet)
 from rest_framework.routers import DefaultRouter
+
+from recipes.views import IngredientViewSet, RecipeViewSet, TagViewSet
 from users.views import SubscriptionViewSet, UserViewSet
 
 router_v1 = DefaultRouter()
@@ -12,13 +12,7 @@ router_v1.register(r'ingredients', IngredientViewSet),
 
 urlpatterns = [
     path('', include(router_v1.urls)),
-    re_path(r'recipes/(?P<recipe_id>\d+)/shopping_cart/', CartViewSet.as_view(
-        {'post': 'create', 'delete': 'destroy'}
-    )),
     re_path(r'users/(?P<user_id>\d+)/subscribe', SubscriptionViewSet.as_view(
-        {'post': 'create', 'delete': 'destroy'}
-    )),
-    re_path(r'recipes/(?P<recipe_id>\d+)/favorite/', FavoriteViewSet.as_view(
         {'post': 'create', 'delete': 'destroy'}
     )),
     path('auth/', include('djoser.urls.authtoken')),
