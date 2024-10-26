865632e3dc0ac0115f3cf65db9ff10733e465cdc
Артём Козлов <shtil14@mail.ru>
2024-10-26 16:02:48 +0300

    add objects counter

--- backend/recipes/management/commands/import_json.py
+++ backend/recipes/management/commands/import_json.py
@@ -3,7 +3,9 @@ import json
 from django.contrib.auth import get_user_model
 from django.contrib.auth.hashers import make_password
 from django.core.management.base import BaseCommand
-from recipes.models import Favorite, Ingredient, Recipe, ShoppingCart, Tag
+
+from recipes.models import (Favorite, Ingredient, Recipe, RecipeIngredient,
+                            RecipeTag, ShoppingCart, Tag)
 from users.models import Subscription
 
 User = get_user_model()
@@ -11,7 +13,8 @@ User = get_user_model()
 classnames = {
     "Ingredient": Ingredient, "Tag": Tag,
     "User": User, "Subscription": Subscription, "Recipe": Recipe,
-    "Favorite": Favorite, "Cart": ShoppingCart
+    "Favorite": Favorite, "Cart": ShoppingCart, "Ri": RecipeIngredient,
+    "Rt": RecipeTag
 }
 
 
@@ -27,6 +30,7 @@ class Command(BaseCommand):
         filepath = kwargs.get('filepath')
         name = kwargs.get('classname').lower().capitalize()
         model = classnames.get(name)
+        counter = 0
 
         with open(filepath, 'r') as json_file:
             data = json.load(json_file)
@@ -34,5 +38,8 @@ class Command(BaseCommand):
                 data['password'] = make_password(data['password'])
             else:
                 for item in data:
+
                     instance = model(**item)
                     instance.save()
+                    counter += 1
+            print(f'Было создано объектов: {counter}')
